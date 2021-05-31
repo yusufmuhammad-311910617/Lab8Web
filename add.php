d338e97d166348c067b63c4ffbdcf0492101e0ad
yusufmuhammad-311910617 <81587959+yusufmuhammad-311910617@users.noreply.github.com>
2021-06-01 00:05:06 +0700

    Add files via upload

---
+++ add.php
@@ -0,0 +1,50 @@
+<html>
+<head>
+	<title>Add Users</title>
+</head>
+ 
+<body>
+	<a href="index.php">Go to Home</a>
+	<br/><br/>
+ 
+	<form action="add.php" method="post" name="form1">
+		<table width="25%" border="0">
+			<tr> 
+				<td>Name</td>
+				<td><input type="text" name="name"></td>
+			</tr>
+			<tr> 
+				<td>Email</td>
+				<td><input type="text" name="email"></td>
+			</tr>
+			<tr> 
+				<td>No_tlp</td>
+				<td><input type="text" name="No_tlp"></td>
+			</tr>
+			<tr> 
+				<td></td>
+				<td><input type="submit" name="Submit" value="Add"></td>
+			</tr>
+		</table>
+	</form>
+	
+	<?php
+ 
+	// Check If form submitted, insert form data into users table.
+	if(isset($_POST['Submit'])) {
+		$name = $_POST['name'];
+		$email = $_POST['email'];
+		$mobile = $_POST['No_tlp'];
+		
+		// include database connection file
+		include_once("config.php");
+				
+		// Insert user data into table
+		$result = mysqli_query($mysqli, "INSERT INTO users(name,email,No_tlp) VALUES('$name','$email','$No_tlp')");
+		
+		// Show message when user added
+		echo "User added successfully. <a href='index.php'>View Users</a>";
+	}
+	?>
+</body>
+</html>
